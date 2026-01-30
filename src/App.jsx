acdd6fe32a12006ed616c7ae63ad781bf247077f
Lalit <rootxl20112@gmail.com>
2026-01-30 23:33:48 +0530

    Refactor navbar and project section layout in App component

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,18 @@ function App() {
 
   return (
     <>
-    {/* navbar */}
-    <div className='flex justify-between bg-blue-900 sticky'>
-      <div>BR Architects</div>
-      <div className='flex gap-4 '>
-        <a href="#">Projects</a>
+    
+    <div className='fixed top-0 z-100 w-full flex justify-between bg-blue-900 h-25 px-6 py-4'>
+      <div className=' text-white border-white-4 border-2 h-42 w-40 text-center backdrop-blur-2xl pt-2.5 '>BR Architects</div>
+      <div className='flex gap-4 pt-8 mt-8 '>
+        <a href="# " className='mt-8' >Projects</a>
         <a href="#">About</a>
         <a href="#">Contact</a>
       </div>
     </div>
     {/* main section  */}
 
-    <div className='relative'>
+    <div className='relative '>
       <img src="../image.png" alt="" className='w-full h-auto ' />
       <span className=' absolute inset-0 flex items-center justify-center font-bold text-white '>
         <span className='bg-black bg-opacity-0 text-2xl py-1 rounded '>BR </span> Architects
@@ -30,16 +30,25 @@ function App() {
     <div>
       <h3>Projects</h3>
       <div className='flex gap-4 '>
-        <div className='relative w1/4 '>
-          <img src="../image.png" alt="" className='w-1/4 h-auto' />
-          <span className=' text-1xl inset-0 absolute'>Summer House</span>
-          
+        <div className='relative '>
+          <img src="../image.png" alt="" className=' border-2  bg-black' />
+          <span className=' inset-0 absolute border-2'> <span className='border-2'>Summer House</span></span>
         </div>
-        <img src="../image.png" alt="" className='w-1/4 h-auto ' />
-        <img src="../image.png" alt="" className='w-1/4 h-auto' />
-        <img src="../image.png" alt="" className='w-1/4 h-auto' />
+        <div className='relative'>
+        <img src="../image.png" alt="" className=' ' />
+        <h3 className='inset-0 absolute border-2'> summer house</h3>
+        </div>
+        <div className='relative'>
+        <img src="../image.png" alt="" className=' ' />
+        <span className='font-extrabold'>sumer house</span>
+        </div>
+        <div className='relative'>
+        <img src="../image.png" alt="" className=' ' />
+        </div>
+        
       </div>
     </div>
+    <h3>root</h3>
     </>
   )
 }
